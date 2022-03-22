38ac3bcb2358a6afb363e3c6bd416be6e59f0af8
David Stockdale <dstock3@gmail.com>
2022-03-21 20:23:42 -0500

    Added a router.post block in the index module to enable post functionality.

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,11 @@ router.get('/new', function(req, res, next) {
   res.render('form');
 });
 
+//Post messages
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.msg, user: req.body.name, added: new Date()})
+
+  res.redirect('/') 
+});
+
 module.exports = router;
